dfb44198ca9db2a9d970c8239c1f4081c351a3f0
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2023-10-04 14:50:49 +0000

    ocean-frontend-outubro-23

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 
+ 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+
     <>
-      <div>
-        <h2> Rick Sanchez><h2/>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      <div/>
+
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+
     </>
+
   )
+
 }
+
+ 
 
 export default App
