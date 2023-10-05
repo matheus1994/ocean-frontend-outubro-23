151122c0692152ed78dc28940f1dadcf7141cc67
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2023-10-05 13:35:20 +0000

    ocean-frontend-outubro-23

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import Card from './components/Card/Card'
 
 function App() {
 
+  const personagem1 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const personagem2 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
   return (
 
     <>
-       <Card/>
-     
+       <Card item={personagem1}/>
+       <Card item={personagem2}/>
+
     </>
 
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,15 @@
 import './Card.css'
 
-export default function Card(){
+export default function Card(props){
+
+    //console.log(props);
+    const item = props.item;
+    //console.log(item);
+
     return(
         <div className="card">
-           <h2>Rick Sanchez</h2>
-           <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+           <h2>{item.nomw}</h2>
+           <img src={item.imagemUrl}/>
         </div>
     )
 }
