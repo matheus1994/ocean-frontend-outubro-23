135cf741e5d2b3e6985a09c2d3dff1f206bc2e45
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2023-10-05 15:17:06 +0000

    ocean-frontend-outubro-23

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,6 +8,13 @@ export default function Card(props){
 
     return(
         <div className="card">
+           <div>
+            <div className="tag">Texto qualquer</div>
+            <div className="tag">Texto qualquer</div>
+            <div className="tag">Texto qualquer</div>
+            <div className="tag">Texto qualquer</div>
+           </div>
+
            <h2>{item.nomw}</h2>
            <img src={item.imagemUrl}/>
         </div>
